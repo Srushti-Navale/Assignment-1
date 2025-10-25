4abc5bf8d03916864b5f7b2e201c8f9dcdf890ac
Srushti-Navale <srushtinavale512@gmail.com>
2025-10-25 16:17:51 +0530

    practice code

--- practicesll.c
+++ practicesll.c
@@ -8,271 +8,278 @@ struct node
 };
 
 void traverse(struct node **head);
-void insertathead(struct node **head,struct node **tail,int data);
-void insertatend(struct node **head ,struct node **tail,int data);
-void insertatposition(struct node **head,struct node **tail,int data,int position);
-void deleteathead(struct node **head,struct node **tail,int *ddata);
-void deleteatend(struct node **head,struct node **tail,int *ddata);
-void deleteatposition(struct node **head,struct node **tail,int position,int *ddata);
-
+void insertathead(struct node **head, struct node **tail, int data);
+void insertatend(struct node **head, struct node **tail, int data);
+void insertatposition(struct node **head, struct node **tail, int data, int position);
+void deleteathead(struct node **head, struct node **tail, int *ddata);
+void deleteatend(struct node **head, struct node **tail, int *ddata);
+void deleteatposition(struct node **head, struct node **tail, int position, int *ddata);
 
 int main()
 {
     struct node *head = NULL;
     struct node *tail = NULL;
     int data = 0;
-    char choice='n';
-    int position=0;
+    char choice = 'y';
+    int position = 0;
 
+    // Input initial list
     do
     {
-        printf("enter node data");
-        scanf("%d",&data);
-        struct node *newnode = (struct node*)malloc(sizeof(struct node));
-        newnode->data=data;
-        if(head==NULL)
+        printf("Enter node data: ");
+        scanf("%d", &data);
+
+        struct node *newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = data;
+        newnode->next = NULL;
+
+        if (head == NULL)
         {
-            head=newnode;
-            tail=newnode;
+            head = newnode;
+            tail = newnode;
         }
         else
         {
-        tail->next= newnode;
-        tail = newnode;
+            tail->next = newnode;
+            tail = newnode;
         }
-        printf("do you want to continue ?(y/n)");
-        scanf("% c",&choice);
-    } while (choice!='y');
 
-    int ch=0;
+        printf("Do you want to continue (y/n)? ");
+        scanf(" %c", &choice); // space before %c to consume newline
+
+    } while (choice == 'y' || choice == 'Y');
+
+    int ch = 0;
     do
     {
-        printf("/n menu/n");
-        printf("1.display/n");
-        printf("2.insert at head/n");
-        printf("3.insert at end/n");
-        printf("4.insert at position/n");
-        printf("5.delete at head/n");
-        printf("6.delete at end/n");
-        printf("7.delete at position/n");
-        printf("0.exit/n");
-        printf("enter your choice");
-        scanf("%d",&ch);
+        printf("\nMenu\n");
+        printf("1. Display\n");
+        printf("2. Insert at head\n");
+        printf("3. Insert at end\n");
+        printf("4. Insert at position\n");
+        printf("5. Delete at head\n");
+        printf("6. Delete at end\n");
+        printf("7. Delete at position\n");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
 
-        switch(ch)
+        switch (ch)
         {
-            case 1:
-                traverse(&head);
-                break;
-            case 2:
-                printf("which data you want to enter at head of list :");
-                scanf("%d",&data);
-                insertathead(&head,&tail,data);
-                break;
-            case 3:
-                printf("which data you want to enter at end of list :");
-                scanf("%d",&data);
-                insertatend(&head,&tail,data);
-                break;
-            case 4:
-                printf("which data you want to enter :");
-                scanf("%d",&data);
-                printf("at which position you want to enter :");
-                scanf("%d",&position);
-                insertatposition(&head,&tail,data,position);
-                break;
-            case 5:
-                deleteathead(&head,&tail,&data);
-                printf("deleted data is %d",data);
-                break;
-            case 6:
-                deleteatend(&head,&tail,&data);
-                printf("deleted data is %d",data);
-                break;
-            case 7:
-                printf("at which position you want to delete :");
-                scanf("%d",&position);
-                deleteatposition(&head,&tail,position,&data);
-                printf("deleted data is %d",data);
-                break;  
-            case 0:
-                printf("exiting...");
-                break;
+        case 1:
+            traverse(&head);
+            break;
+        case 2:
+            printf("Enter data to insert at head: ");
+            scanf("%d", &data);
+            insertathead(&head, &tail, data);
+            break;
+        case 3:
+            printf("Enter data to insert at end: ");
+            scanf("%d", &data);
+            insertatend(&head, &tail, data);
+            break;
+        case 4:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            printf("Enter position (0-based): ");
+            scanf("%d", &position);
+            insertatposition(&head, &tail, data, position);
+            break;
+        case 5:
+            deleteathead(&head, &tail, &data);
+            printf("Deleted data is %d\n", data);
+            break;
+        case 6:
+            deleteatend(&head, &tail, &data);
+            printf("Deleted data is %d\n", data);
+            break;
+        case 7:
+            printf("Enter position to delete: ");
+            scanf("%d", &position);
+            deleteatposition(&head, &tail, position, &data);
+            printf("Deleted data is %d\n", data);
+            break;
+        case 0:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice!\n");
         }
-    } while (ch!=0);
-    
+    } while (ch != 0);
+
     return 0;
 }
 
-
 void traverse(struct node **head)
 {
-    if(head == NULL)
+    if (*head == NULL)
     {
-        printf("list is empty");
-    }else{
-        struct node *temp=*head;
-        while(temp!=NULL)
-        {
-            printf("%d",temp->data);
-            temp=temp->next;
-        }
-    }
-    
-}
-
-
-void insertathead(struct node **head,struct node **tail,int data)
-{
-    struct node *newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->data=data;
-    if(*head == NULL)
-   {
-      *head=newnode;
-      *tail = newnode;
-   }
-   else
-   {
-     newnode->next=*head;
-     *head=newnode;
-   }
-   traverse(&head);
-}
-
-void insertatend(struct node **head ,struct node **tail,int data)
-{
-    struct node *newnode=(struct node *)malloc(sizeof(struct node));
-    newnode->data=data;
-
-    if(*head == NULL)
-   {
-      *head=newnode;
-      *tail = newnode;
-   }
-   else
-   {
-      newnode->next=NULL;
-      (*tail)->next=newnode;
-      *tail=newnode;
-   }
-    traverse(&head);
-}
-
-void insertatposition(struct node **head,struct node **tail,int data,int position)
-{
-    struct node *newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->data=data;
-
-    if(position==0)
-    {
-        insertathead(&head,&tail,data);
+        printf("List is empty.\n");
     }
     else
     {
         struct node *temp = *head;
-        for(int i=0;i<position-1;i++)
+        printf("List: ");
+        while (temp != NULL)
         {
-            temp=temp->next;
-            if(temp==NULL)
-            {
-                printf("position is invalid");
-                i=position+1;
-            }
+            printf("%d -> ", temp->data);
+            temp = temp->next;
         }
-
-        if(temp!=NULL)
-        {
-            newnode->next=temp->next;
-            temp->next=newnode;
-        }
+        printf("NULL\n");
     }
-    traverse(&head);
 }
 
-void deleteathead(struct node **head,struct node **tail,int *ddata)
+void insertathead(struct node **head, struct node **tail, int data)
 {
-    if(*head == NULL)
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = *head;
+
+    *head = newnode;
+    if (*tail == NULL)
+        *tail = newnode;
+
+    traverse(head);
+}
+
+void insertatend(struct node **head, struct node **tail, int data)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = NULL;
+
+    if (*head == NULL)
     {
-        printf("list is empty");
+        *head = newnode;
+        *tail = newnode;
     }
     else
     {
-        struct node *temp = *head;
-
-        *head=(*head)->next;
-        *ddata = temp->data;
-        free(temp);
+        (*tail)->next = newnode;
+        *tail = newnode;
     }
-    traverse(&head);
+    traverse(head);
 }
 
-void deleteatend(struct node **head,struct node **tail,int *ddata)
+void insertatposition(struct node **head, struct node **tail, int data, int position)
 {
-    if(*head == NULL)
+    if (position == 0)
     {
-        printf("list is empty");
+        insertathead(head, tail, data);
+        return;
+    }
+
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->next = NULL;
+
+    struct node *temp = *head;
+    for (int i = 0; i < position - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Invalid position!\n");
+        free(newnode);
+        return;
+    }
+
+    newnode->next = temp->next;
+    temp->next = newnode;
+
+    if (newnode->next == NULL)
+        *tail = newnode;
+
+    traverse(head);
+}
+
+void deleteathead(struct node **head, struct node **tail, int *ddata)
+{
+    if (*head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct node *temp = *head;
+    *ddata = temp->data;
+    *head = (*head)->next;
+
+    if (*head == NULL)
+        *tail = NULL;
+
+    free(temp);
+    traverse(head);
+}
+
+void deleteatend(struct node **head, struct node **tail, int *ddata)
+{
+    if (*head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct node *temp = *head;
+
+    if ((*head)->next == NULL)
+    {
+        *ddata = (*head)->data;
+        free(*head);
+        *head = NULL;
+        *tail = NULL;
     }
     else
     {
-        struct node *temp = *head;
-        if((*head)->next == NULL)
+        while (temp->next != *tail)
         {
-            free(*head);
-            *head = NULL;
-            *tail = NULL;
+            temp = temp->next;
         }
-        else
-        {
-            while(temp->next != *tail)
-            {
-                temp=temp->next;
-            }
-            *ddata = (*tail)->data;
-            free(*tail);
-            temp->next=NULL;
-            *tail=temp;
-        }
+
+        *ddata = (*tail)->data;
+        free(*tail);
+        *tail = temp;
+        (*tail)->next = NULL;
     }
-    traverse(&head);
+    traverse(head);
 }
 
-void deleteatposition(struct node **head,struct node **tail,int position,int *ddata)
+void deleteatposition(struct node **head, struct node **tail, int position, int *ddata)
 {
-    if(*head == NULL)
+    if (*head == NULL)
     {
-        printf("list is empty");
+        printf("List is empty.\n");
+        return;
     }
-    else
-    {
-        if(position==0)
-        {
-            deleteathead(&head,&tail,&ddata);
-        }
-        else
-        {
-            struct node *temp = *head;
-            for(int i=0;i<position-1;i++)
-            {
-                temp=temp->next;
-                if(temp==NULL)
-                {
-                    printf("position is invalid");
-                    i=position+1;
-                }
-            }
 
-            if(temp!=NULL && temp->next != NULL)
-            {
-                struct node *todelete = temp->next;
-                temp->next = temp->next->next;
-                if(todelete == *tail)
-                {
-                    *tail = temp;
-                }
-                *ddata = todelete->data;
-                free(todelete);
-            }
-        }
+    if (position == 0)
+    {
+        deleteathead(head, tail, ddata);
+        return;
     }
-    traverse(&head);
+
+    struct node *temp = *head;
+    for (int i = 0; i < position - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL || temp->next == NULL)
+    {
+        printf("Invalid position!\n");
+        return;
+    }
+
+    struct node *todelete = temp->next;
+    *ddata = todelete->data;
+    temp->next = todelete->next;
+
+    if (todelete == *tail)
+        *tail = temp;
+
+    free(todelete);
+    traverse(head);
 }
